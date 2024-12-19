e0682a93775a2a1c384ed3eb3eff2eb26d7234da
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2024-12-19 16:46:12 -0300

    test

--- tap_airbyte/yarn/main.py
+++ tap_airbyte/yarn/main.py
@@ -115,14 +115,14 @@ def _get_yarn_service_app_id(yarn_config: YarnConfig, service_uri: str) -> str:
     app_id = None
     state = None
     logger.debug('Waiting for the application id...')
-    while not app_id or state not in {'RUNNING', *YARN_APP_TERMINAL_STATES}:
+    while not app_id or state not in {'STARTED', 'SUCCEEDED'}:
         logger.info(f'APP_ID: {app_id}, STATE: {state}')
         response = session.get(url)
         app_info = response.json()
         app_id = app_info.get('id')
         state = app_info.get('state', 'STOPPED')
-        if state == 'STOPPED':
-            raise Exception(f"Yarn Service stopped before start the application: {response.json()}")
+        if state in {'STOPPED', 'FAILED'}:
+            raise Exception(f"Yarn Service stopped/failed before start the application: {response.json()}")
         sleep(1) # control the requests
     logger.info(f'APP_ID: {app_id}, STATE: {state}')
     return app_id
