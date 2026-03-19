aa427d857174e260ee089ab385e22a877dbce7d4
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2026-03-19 17:33:36 -0300

    remove test

--- tap_airbyte/tap.py
+++ tap_airbyte/tap.py
@@ -574,7 +574,6 @@ class TapAirbyte(Tap):
             )
             try:
                 # Context is held until EOF or exception
-                raise Exception("Simulated crash for testing YARN app kill logic.")
                 yield proc
             finally:
                 if not self.eof_received:
